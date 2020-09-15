e55a8a7d81909ed98069bc3e7c14bfd5b5f57a49
Marrione Nguyen <mtiamsic@gmail.com>
2020-09-15 14:55:35 -0700

    validate answers

--- app.js
+++ app.js
@@ -18,12 +18,28 @@ const promptUser = () => {
         {
             type: 'input',
             name: 'name',
-            message: 'What is your name?'
+            message: 'What is your name?',
+            validate: nameInput => {
+                if (nameInput) {
+                    return true;
+                } else {
+                    console.log('Please enter your name!');
+                    return false;
+                }
+            }
         },
         {
             type: 'input',
             name: 'github',
-            message: 'Enter your GitHub Username'
+            message: 'Enter your GitHub Username',
+            validate: nameInput => {
+                if (nameInput) {
+                    return true;
+                } else {
+                    console.log('Please enter your GitHub userame!');
+                    return false;
+                }
+            }
         },
         {
             type: 'input',
@@ -44,17 +60,33 @@ const promptProject = portfolioData => {
   Add a New Project
   =================
   `);
-  
+
     return inquirer.prompt([
         {
             type: 'input',
             name: 'name',
-            message: 'What is the name of your project?'
+            message: 'What is the name of your project?',
+            validate: nameInput => {
+                if (nameInput) {
+                    return true;
+                } else {
+                    console.log('Please enter the name of your project!');
+                    return false;
+                }
+            }
         },
         {
             type: 'input',
             name: 'description',
-            message: 'Provide a description of the project (Required)'
+            message: 'Provide a description of the project (Required)',
+            validate: nameInput => {
+                if (nameInput) {
+                    return true;
+                } else {
+                    console.log('Please provide a description of the project!');
+                    return false;
+                }
+            }
         },
         {
             type: 'checkbox',
@@ -65,7 +97,15 @@ const promptProject = portfolioData => {
         {
             type: 'input',
             name: 'link',
-            message: 'Enter the GitHub link to your project. (Required)'
+            message: 'Enter the GitHub link to your project. (Required)',
+            validate: nameInput => {
+                if (nameInput) {
+                    return true;
+                } else {
+                    console.log('Please enter the GitHub link to your project!');
+                    return false;
+                }
+            }
         },
         {
             type: 'confirm',
