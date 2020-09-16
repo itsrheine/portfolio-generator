1035c57a1af27ab467411ebb93f33845b4ec5620
Marrione Nguyen <mtiamsic@gmail.com>
2020-09-16 15:00:37 -0700

    updated changes

--- app.js
+++ app.js
@@ -8,7 +8,7 @@
 //     console.log('Portfolio complete! Check out index.html to see the output!');
 // })
 
-const fs = require('fs');
+const { writeFile, copyFile } = require('./utils/generate-site.js');
 const generatePage = require('./src/page-template.js'); //assigns the anonymous HTML template function in page-template.js to the variable generatePage.
 const inquirer = require("inquirer");
 
@@ -137,11 +137,18 @@ const promptProject = portfolioData => {
 promptUser()
     .then(promptProject)
     .then(portfolioData => {
-        const pageHTML = generatePage(portfolioData);
-
-        fs.writeFile('./index.html', pageHTML, err => {
-            if (err) throw new Error(err);
-
-            console.log('Page created! Check out index.html in this directory to see it!');
-        });
+        return generatePage(portfolioData);
+    })
+    .then(pageHTML => {
+        return writeFile(pageHTML);
+    })
+    .then(writeFileResponse => {
+        console.log(writeFileResponse);
+        return copyFile();
+    })
+    .then(copyFileResponse => {
+        console.log(copyFileResponse);
+    })
+    .catch(err => {
+        console.log(err);
     });
